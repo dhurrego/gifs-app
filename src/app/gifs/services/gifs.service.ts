6fc7efed926972a0b6def15e7f79cd3e2d5a712c
Deivid Urrego <deivid_199615@hotmail.com>
2021-01-24 22:40:50 -0500

    Se crean los metodos necesarios para consumir el servicio de Giphy y se crea la interface con los datos de la respuesta del get

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Gif, SearchGifsResponse } from '../gifs-page/interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
-  private _historial: string[] = []
+  private apiKey: string = 'X5umd4WtHGlA6A43ZPYINNBYv1q6MNbL';
+  private _historial: string[] = [];
+
+  public resultados: Gif[] = [];
 
   get historial() {
     return [...this._historial];
   }
-  constructor() { }
-  
+
+  constructor( private _http: HttpClient ) { }
+
   buscarGifs( query: string = ''){
     
     query = query.trim().toLocaleLowerCase();
@@ -19,6 +25,15 @@ export class GifsService {
     if( !this._historial.includes( query )){
       this._historial.unshift( query );
       this._historial = this._historial.splice(0, 10);
+      this._http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=${ this.apiKey }&q=${ query }&limit=10`).subscribe(
+        response => {
+          console.log(response.data);
+          this.resultados = response.data;
+        },
+        error => {
+          console.log( <any>error );
+        }
+      )
     }
 
     console.log(this._historial);
